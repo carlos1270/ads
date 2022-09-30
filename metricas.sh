7729118f552cb65337332ccff1171f60508e198f
Carlos VM3 <carlos.andre1270@gmail.com>
2022-09-30 00:09:48 +0000

    files conflict

--- metricas.sh
+++ metricas.sh
@@ -1,8 +1,8 @@
 # Variaveis importantes
-log=logs/log_metricas.txt
+log=logs/log_vm3_metricas5_750.txt
 cont=1
-disk=sda5
-placa=wlo
+disk=sda2
+placa=enp
 
 # Cabeçalho
 echo Cont MenUsed MenFree MenShared SwapUsed SwapFree DiskBlockKB DiskUsed DiskUsedPerc CpuUsr CpuSys CpuIOWait CpuSoft CpuIdle NetBytesReceived NetPackagesReceived NetErrsReceived NetBytesTransmitid NetPackagesTransmitid NetErrsTransmitid Data Hora > $log
